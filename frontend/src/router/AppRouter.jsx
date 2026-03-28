7363356a91e244eb7a575c7ad17ae0a121e88f49
Hitesh <hoga.hitesh@gmail.com>
2026-03-28 18:53:59 +0530

    fix landing page

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -8,8 +8,8 @@ import WorkspacePlaceholderPage from "../pages/WorkspacePlaceholderPage";
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<WelcomePage />} />
-      <Route path="/testops" element={<TestOpsLandingPage />} />
+      <Route path="/" element={<TestOpsLandingPage />} />
+      <Route path="/onboarding" element={<WelcomePage />} />
       <Route path="/login/:mode" element={<LoginPage />} />
       <Route path="/workspace" element={<WorkspacePlaceholderPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
